1eb9ea5a9ec84ff8971c6ddaa6f0d790b8003cc6
Peeem3201 <pimmy3201@gmail.com>
2025-08-16 23:50:32 +0700

    intial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+    
+    return (  
+        <div classname= 'background'>
+            <div className='frame'>
+                <div className='profile justify-items-center'>
+                    <div className='profile_pic'>
+                    <div className='profile txt1'>สวัสดี</div>
+                    <div className='profile txt2'>คุณ หมูเด้ง</div>
+                </div>
+                </div>
+            </div>
+        </div>
+        
+    )
+}
+
+export default App
