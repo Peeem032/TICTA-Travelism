9b23eae36ea898af744f387c48979b0e947774c5
Peeem3201 <pimmy3201@gmail.com>
2025-08-17 03:26:30 +0700

    Implement home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,12 +4,30 @@ function App() {
     
     return (  
         <div classname= 'background'>
-            <div className='frame'>
-                <div className='profile justify-items-center'>
-                    <div className='profile_pic'>
-                    <div className='profile txt1'>สวัสดี</div>
-                    <div className='profile txt2'>คุณ หมูเด้ง</div>
+            <div className='frame flex flex-col'>
+                <div className='profile'>
+                    <div className='profile_pic'></div>
+                    <div className='flex flex-col mt-4'>
+                        <span className='profile_txt1'>สวัสดี</span>
+                        <span className='profile_txt2 text-base'>คุณ <span className='font-bold'>หมูเด้ง</span>  </span>
+                    </div>
                 </div>
+                <div className='setting_icon'></div>
+                <div className='box flex flex-row '>
+                    
+                    <div className="icon">
+                        <img src="/assets/picture/icon_ticket.png" alt="ticket"/>
+                        <span>ส่วนลด</span>
+                    </div>
+                    <div className="icon">
+                        <img src="/assets/picture/icon_gif.png" alt="gif"/>
+                        <span>รางวัล</span>
+                    </div>
+                     <div className="icon">
+                        <img src="/assets/picture/icon_add.png" alt="add"/>
+                        <span>เพิ่มเติม</span>
+                    </div>
+
                 </div>
             </div>
         </div>
